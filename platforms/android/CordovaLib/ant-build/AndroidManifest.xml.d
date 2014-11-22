866e3e04ec0b2987e2db928435c262e66f9f13b5
hancoma <hancoma@gmail.com>
2014-11-23 03:44:03 +0900

    신규등록
    신규등록

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-C:\Users\ch7_000\Documents\GitHub\rococophotoapp\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-C:\Users\ch7_000\Documents\GitHub\rococophotoapp\platforms\android\CordovaLib\AndroidManifest.xml \
+C:\Users\ch7_000\Dropbox\GitHub\rococophotoapp\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\Users\ch7_000\Dropbox\GitHub\rococophotoapp\platforms\android\CordovaLib\AndroidManifest.xml \
--- platforms/android/ant-build/AndroidManifest.xml.d
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-C:\Users\ch7_000\Documents\GitHub\rococophotoapp\platforms\android\ant-build\AndroidManifest.xml : \
-C:\Users\ch7_000\Documents\GitHub\rococophotoapp\platforms\android\AndroidManifest.xml \
+C:\Users\ch7_000\Dropbox\GitHub\rococophotoapp\platforms\android\ant-build\AndroidManifest.xml : \
+C:\Users\ch7_000\Dropbox\GitHub\rococophotoapp\platforms\android\AndroidManifest.xml \
